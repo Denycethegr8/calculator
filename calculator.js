09a2fc72ff63992056bd4b3b2cb3408178da54dc
denyce <jini3209@gmail.com>
2022-10-12 21:43:36 +0900

    slowly improving function

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ const displayBttns = Array.from(document.querySelectorAll('.numbers-operators > 
 const numbers = Array.from(document.querySelectorAll('.numbers')).map(e => e.addEventListener('click', saveVar));
 
 let var1 = '';
-let var2 = '';
+let var2 = 2;
 
 function displayEvent(){
     display.textContent += this.textContent;
@@ -16,16 +16,20 @@ function saveVar(){
     console.log(var1);
 }
 
+let operatorSelect
+
 function setOperator(){
     console.log(this.id);
-    return this.id;
+    operatorSelect = this.id;
 }
 
 function whichOperator(){
-    if(setOperator === 'plus') return plus(var1, var2);
-    else if (setOperator === 'minus') return minus(var1, var2);
-    else if (setOperator === 'multiply') return multiply(var1, var2);
-    else return divide(var1, var2)
+    console.log(operatorSelect);
+    console.log(var2)
+    if(operatorSelect === 'plus') display.textContent = plus(var1, var2);
+    else if (operatorSelect === 'minus') display.textContent =  minus(var1, var2);
+    else if (operatorSelect === 'multiply') display.textContent =  multiply(var1, var2);
+    else display.textContent = divide(var1, var2)
 }
 
 function plus(a, b){
@@ -44,12 +48,22 @@ function divide(a, b){
     return a/b;
 };
 
-document.getElementById('divide').addEventListener('click', setOperator) 
+Array.from(document.querySelectorAll('.operators:not(#equals)')).map(e => e.addEventListener('click', setOperator));
 
-document.getElementById('multiply').addEventListener('click', setOperator) 
+// document.getElementById('divide').addEventListener('click', setOperator) 
 
-document.getElementById('minus').addEventListener('click', setOperator) 
+// document.getElementById('multiply').addEventListener('click', setOperator) 
 
-document.getElementById ('plus').addEventListener('click', setOperator) 
+// document.getElementById('minus').addEventListener('click', setOperator) 
 
-document.getElementById('equals').addEventListener('click', setOperator)
+// document.getElementById ('plus').addEventListener('click', setOperator) 
+
+// document.getElementById('equals').addEventListener('click', whichOperator)
+
+document.querySelector('#clear').addEventListener('click', clear)
+
+function clear(){
+    display.textContent = '';
+    var1 = '';
+    operatorSelect = '';
+}
