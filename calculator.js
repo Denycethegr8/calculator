74fac18cdf966713eb97f4ed9887dc1d5b15d608
denyce <jini3209@gmail.com>
2022-10-13 21:52:59 +0900

    need to figure out how to store two variables

--- calculator.js
+++ calculator.js
@@ -1,19 +1,22 @@
 const display = document.querySelector('#display');
 
-const displayBttns = Array.from(document.querySelectorAll('.numbers-operators > button')).map(e => e.addEventListener('click', displayEvent));
+const displayBttns = Array.from(document.querySelectorAll('.numbers-operators > button:not(#equals)')).map(e => e.addEventListener('click', displayEvent));
 
 const numbers = Array.from(document.querySelectorAll('.numbers')).map(e => e.addEventListener('click', saveVar));
 
-let var1 = '';
-let var2 = 2;
+const numberArr = Array.from(document.querySelectorAll('.numbers')).map(e => e.id);
+console.log(numberArr);
+
+let var1 = [];
+let var2 = '';
 
 function displayEvent(){
     display.textContent += this.textContent;
 };
 
 function saveVar(){
-    var1 += this.textContent;
-    console.log(var1);
+    var1.push(this.textContent);
+    console.log(var1)
 }
 
 let operatorSelect
@@ -21,11 +24,11 @@ let operatorSelect
 function setOperator(){
     console.log(this.id);
     operatorSelect = this.id;
+    display.textContent = '';
 }
 
 function whichOperator(){
     console.log(operatorSelect);
-    console.log(var2)
     if(operatorSelect === 'plus') display.textContent = plus(var1, var2);
     else if (operatorSelect === 'minus') display.textContent =  minus(var1, var2);
     else if (operatorSelect === 'multiply') display.textContent =  multiply(var1, var2);
@@ -49,16 +52,6 @@ function divide(a, b){
 };
 
 Array.from(document.querySelectorAll('.operators:not(#equals)')).map(e => e.addEventListener('click', setOperator));
-
-// document.getElementById('divide').addEventListener('click', setOperator) 
-
-// document.getElementById('multiply').addEventListener('click', setOperator) 
-
-// document.getElementById('minus').addEventListener('click', setOperator) 
-
-// document.getElementById ('plus').addEventListener('click', setOperator) 
-
-// document.getElementById('equals').addEventListener('click', whichOperator)
 
 document.querySelector('#clear').addEventListener('click', clear)
 
